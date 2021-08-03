20b11697b0293ba5521f5924ad2422bb12e7350a
Byron Batteson <byronbatteson@gmail.com>
2021-08-03 17:53:58 +0200

    Add root git directory name to zsh prompt

--- zsh/.config/zsh/config.d/prompt.zsh
+++ zsh/.config/zsh/config.d/prompt.zsh
@@ -1,6 +1,7 @@
 function git_status() {
     local git_branch=$(git symbolic-ref HEAD 2> /dev/null | cut -d'/' -f3)
     local git_status=$(git status 2> /dev/null)
+    local git_root=$(basename `git rev-parse --show-toplevel`)
 
     local git_color="green"
 
@@ -13,7 +14,7 @@ function git_status() {
 
     # Prepare text
     if [[ ! -z $git_branch ]]; then
-        echo "%F{240}git[%f%F{$git_color}$git_branch%f%F{240}]%f"
+        echo "%F{240}git[%f$git_root:%F{$git_color}$git_branch%f%F{240}]%f"
     fi
 }
 
