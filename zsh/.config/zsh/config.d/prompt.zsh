a6da4fa6f99f16eb0e6fcabfc70c3666c62a70e1
Byron Batteson <byronbatteson@gmail.com>
2021-08-05 16:39:45 +0200

    Reduce zsh work for prompt
    Previously, three git commands were run regardless of whether the current
    directory is a repo. Now, it only runs once if not in a git repo.

--- zsh/.config/zsh/config.d/prompt.zsh
+++ zsh/.config/zsh/config.d/prompt.zsh
@@ -2,20 +2,20 @@
 
 function git_status() {
     local git_branch=$(git symbolic-ref HEAD 2> /dev/null | cut -d'/' -f3)
-    local git_status=$(git status 2> /dev/null)
-    local git_root=$(basename `git rev-parse --show-toplevel 2> /dev/null` 2> /dev/null)
-
-    local git_color="green"
-
-    # Determine color
-    if [[ ! $git_status =~ "working tree clean" ]]; then
-        git_color="red"
-    elif [[ $git_status =~ "Your branch is ahead of" ]]; then
-        git_color="yellow"
-    fi
 
     # Prepare text
     if [[ ! -z $git_branch ]]; then
+        local git_status=$(git status 2> /dev/null)
+        local git_root=$(basename `git rev-parse --show-toplevel 2> /dev/null` 2> /dev/null)
+        local git_color="green"
+
+        # Determine color
+        if [[ ! $git_status =~ "working tree clean" ]]; then
+            git_color="red"
+        elif [[ $git_status =~ "Your branch is ahead of" ]]; then
+            git_color="yellow"
+        fi
+
         echo "%F{240}git[%f$git_root:%F{$git_color}$git_branch%f%F{240}]%f"
     fi
 }
