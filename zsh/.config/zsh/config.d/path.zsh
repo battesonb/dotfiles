6312e3a2ece89ff226535246e2d19c10feed5d68
Byron Batteson <byronbatteson@gmail.com>
2021-11-06 08:57:26 +0200

    Update zsh and urxvt config
    Update font size and copy/paste shortcuts for urxvt. Update zsh history,
    path and add Java home env variable.

--- zsh/.config/zsh/config.d/path.zsh
+++ zsh/.config/zsh/config.d/path.zsh
@@ -1,9 +1,12 @@
 # Responsible for amending the environment path variable with required paths
 
+# Dart-related paths
+export PATH="/usr/lib/dart/bin:$PATH"
+
+# Rust-related paths
 export PATH="$HOME/.cargo/bin:$PATH"
 
 # Prepare nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
