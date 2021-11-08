1c9413393d5f33e12b831082c3f5a37fe57e9444
Byron Batteson <byronbatteson@gmail.com>
2021-11-08 18:04:07 +0200

    Update JAVA_HOME for MacOS
    Macs store Java in an alternate directory.

--- zsh/.config/zsh/config.d/java.zsh
+++ zsh/.config/zsh/config.d/java.zsh
@@ -1,1 +1,8 @@
-export JAVA_HOME=/usr/lib/jvm/java-openjdk/
+case `uname` in
+  Darwin)
+    export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-13.jdk/Contents/Home/
+  ;;
+  Linux)
+    export JAVA_HOME=/usr/lib/jvm/java-openjdk/
+  ;;
+esac
