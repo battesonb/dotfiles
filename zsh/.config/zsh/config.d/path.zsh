c2ce1a87a57c17cf125d4af24f9a844098c2cd70
Byron Batteson <byronbatteson@gmail.com>
2022-04-11 10:23:06 +0200

    Add additional path to path env for zsh
    This path contains additional binaries used in my Linux environment.

--- zsh/.config/zsh/config.d/path.zsh
+++ zsh/.config/zsh/config.d/path.zsh
@@ -1,5 +1,7 @@
 # Responsible for amending the environment path variable with required paths
 
+export PATH="$HOME/.local/bin:$PATH"
+
 # Dart-related paths
 export PATH="/usr/lib/dart/bin:$PATH"
 
@@ -10,3 +12,4 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
