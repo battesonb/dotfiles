c43b8a01c2a099dcc6a3ca188af245c488797420
Byron Batteson <byronbatteson@gmail.com>
2022-04-26 07:13:58 +0200

    Add sdkman to path

--- zsh/.config/zsh/config.d/path.zsh
+++ zsh/.config/zsh/config.d/path.zsh
@@ -13,3 +13,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# Export sdkman
+export SDKMAN_DIR="$HOME/.sdkman"
+[[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
