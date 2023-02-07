290ae569c362faae608adf6b0e5b353d8f23c3ac
Byron Batteson <byronbatteson@gmail.com>
2023-02-07 10:38:12 +0200

    Update coc.nvim config for Rust

--- nvim/.config/nvim/plugin/treesitter.lua
+++ nvim/.config/nvim/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 local configs = require("nvim-treesitter.configs")
 local List = require("plenary.collections.py_list")
 
-local languages = { "wgsl" }
+local skipHighlights = { "wgsl" }
 
 function hasValue(table, value)
 	for _, v in ipairs(table) do
@@ -13,14 +13,18 @@ function hasValue(table, value)
 end
 
 configs.setup({
-	ensure_installed = languages,
-	sync_install = false,
-	auto_install = true,
+	ensure_installed = {
+		"rust",
+		"typescript",
+	},
+	sync_install = true,
+	auto_install = false,
+	ignore_install = { "vim" },
 	highlight = {
 		enable = true,
 		disable = function(lang, bufnr)
 			-- To support running both the Neovim LSP and coc.nvim.
-			return not hasValue(languages, lang)
+			return not hasValue(skipHighlights, lang)
 		end
 	}
 })
--- vim/.vim/plugin/coc.vim
+++ vim/.vim/plugin/coc.vim
@@ -19,6 +19,8 @@ let g:coc_global_extensions = [
   \'coc-yaml',
   \]
 
+let g:coc_global_config="~/.vim/coc-settings.json"
+
 " Use <c-space> to trigger completion
 inoremap <silent><expr> <c-space> coc#refresh()
 inoremap <silent><expr> <nul> coc#refresh()
