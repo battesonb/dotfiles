bbeed582d2a94a6a28125b9d329a164a0dc35058
Byron Batteson <byronbatteson@gmail.com>
2023-11-11 08:01:36 +0200

    Use ripgrep for telescope find files command

--- nvim/.config/nvim/plugin/telescope.lua
+++ nvim/.config/nvim/plugin/telescope.lua
@@ -1,9 +1,17 @@
+local builtin = require("telescope.builtin")
 local telescope = require("telescope")
 
 telescope.setup({
 	pickers = {
 		find_files = {
-			hidden = true
+			find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" }
 		}
 	}
 })
+
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
+vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
--- nvim/.config/nvim/plugin/telescope.vim
+++
@@ -1,6 +0,0 @@
-nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
-nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
-nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
-nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
-nnoremap <leader>fm <cmd>lua require('telescope.builtin').man_pages()<cr>
-nnoremap <leader>ft <cmd>lua require('telescope.builtin').treesitter()<cr>
