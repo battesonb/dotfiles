8f36d43be3c59c2a33f430a328071da38540828f
Byron Batteson <byronbatteson@gmail.com>
2024-09-21 08:36:27 +0100

    Return incremental search in zsh

--- zsh/.config/zsh/config.d/behaviour.zsh
+++ zsh/.config/zsh/config.d/behaviour.zsh
@@ -1,4 +1,6 @@
 autoload -U edit-command-line
 zle -N edit-command-line
+bindkey -e
 bindkey '^xe' edit-command-line
 bindkey '^x^e' edit-command-line
+bindkey '^R' history-incremental-search-backward
