26f65519ff9c1e9770866462b81b67fb9e76c5e9
Byron Batteson <byronbatteson@gmail.com>
2024-09-21 08:36:50 +0100

    Update to JDK21 on Mac

--- zsh/.config/zsh/config.d/java.zsh
+++ zsh/.config/zsh/config.d/java.zsh
@@ -1,6 +1,6 @@
 case `uname` in
   Darwin)
-    export JAVA_HOME=/opt/homebrew/opt/openjdk@17/
+    export JAVA_HOME=/opt/homebrew/opt/openjdk@21/
   ;;
   Linux)
     export JAVA_HOME=/usr/lib/jvm/java-openjdk/
