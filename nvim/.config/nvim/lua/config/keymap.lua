7f07867dfe46d871977fb7d3b6544571e3719a48
Byron Batteson <byronbatteson@gmail.com>
2024-12-23 23:11:48 +0000

    Add more nvim keymaps

--- nvim/.config/nvim/lua/config/keymap.lua
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -5,3 +5,7 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")          -- move selection down
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")          -- move selection up
 vim.keymap.set("n", "<leader>j", ":m .+1<CR>==")      -- move line down
 vim.keymap.set("n", "<leader>k", ":m .-2<CR>==")      -- move line up
+vim.keymap.set("x", "<leader>p", "\"_dP")             -- keep value in register on paste
+
+-- Replace word under cursor (see cmdline.txt for more)
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>")
