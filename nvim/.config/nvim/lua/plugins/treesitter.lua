a11c072d2374fc4495ffac3e489393f95d68845b
Byron Batteson <byronbatteson@gmail.com>
2025-04-22 12:23:59 +0100

    Use treesitter indent
    Had a few edge cases around things like doc comments being
    ill-formatted.

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -15,6 +15,9 @@ return {
           enable = true,
           additional_vim_regex_highlighting = false,
         },
+        indent = {
+          enable = true,
+        },
       })
     end
   },
