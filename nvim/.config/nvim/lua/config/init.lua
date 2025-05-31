8b486d608a3fe2b9530e68d3ac0ed536c50e6df4
Byron Batteson <byronbatteson@gmail.com>
2025-05-31 12:49:04 +0100

    Add Neovim Lua override support
    Some devices have local config that I don't want to commit to my
    dotfiles. Generally experiments.

--- nvim/.config/nvim/lua/config/init.lua
+++ nvim/.config/nvim/lua/config/init.lua
@@ -1,3 +1,8 @@
 require("config.autocmds")
 require("config.behavior")
 require("config.keymap")
+
+local nvim_overrides = vim.fn.expand("~/.config/nvim/lua/config/overrides.lua")
+if vim.fn.filereadable(nvim_overrides) == 1 then
+  require("config.overrides")
+end
