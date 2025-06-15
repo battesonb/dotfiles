6c9a41bb9f838a781e091e9cbb651d6be00fb9fe
Byron Batteson <byronbatteson@gmail.com>
2025-06-15 08:45:38 +0100

    Add VS code settings
    Sometimes I have to use VS Code, I'd like to keep the experience at
    least somewhat similar to my Neovim setup.

--- install-win.ps1
+++ install-win.ps1
@@ -2,6 +2,7 @@ $directories = @{
     "vim/.vim" = "$Env:USERPROFILE\vimfiles";
     "intellij/.ideavimrc" = "$Env:USERPROFILE\.ideavimrc";
     "nvim/.config/nvim" = "$Env:LOCALAPPDATA\nvim";
+    "vscode/.config/Code/User/settings.json" = "$Env:APPDATA\Code\User\settings.json";
 }
 
 foreach ($item in $directories.GetEnumerator()) {
