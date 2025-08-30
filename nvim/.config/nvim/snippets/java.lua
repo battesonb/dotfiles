0568e54cac730fb550ac5afc17a677d84831cf28
Byron Batteson <byronbatteson@gmail.com>
2025-08-30 15:24:41 +0100

    Neovim: Add java package snippet

--- nvim/.config/nvim/snippets/java.lua
+++ nvim/.config/nvim/snippets/java.lua
@@ -3,11 +3,29 @@ local utils = require("plugins.snippets.utils")
 
 local c = ls.choice_node
 local i = ls.insert_node
+local d = ls.dynamic_node
 local s = ls.snippet
+local sn = ls.snippet_node
 local t = ls.text_node
 local nl = utils.new_line
 local file_name = utils.file_name
 local ref = utils.ref
+
+-- Turns `.../src/main/java/org/package/Foo.java` into `org.package`. Fallback is `org.example`.
+local function package_name(_, _)
+  local filepath = vim.fn.expand("%:p")
+  local parts = vim.split(filepath, "/")
+  local idx = vim.fn.index(parts, "java")
+  local suggested_package = "org.example"
+  if idx ~= -1 and idx + 1 < #parts then
+    local pkg_parts = vim.list_slice(parts, idx + 2, #parts - 1)
+    suggested_package = table.concat(pkg_parts, ".")
+  end
+
+  return sn(nil, {
+    i(1, suggested_package)
+  })
+end
 
 ls.add_snippets("java", {
   s("catch", {
@@ -60,8 +78,11 @@ ls.add_snippets("java", {
   s("new", {
     i(1, "Type"), t " ", i(2, "name"), t " = new ", ref(1), t "();",
   }),
+  s("package", {
+    t "package ", d(1, package_name, {}), t ";",
+  }),
   s("psvm", {
-    t "public static void main(String[] args) {", nl(),
+    t "public static void main(final String[] args) {", nl(),
     t "\t", i(0), nl(),
     t "}",
   }),
