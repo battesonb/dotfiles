60ea90ceea0ee9345e1a6ce4563cb45616e27b58
Byron Batteson <byronbatteson@gmail.com>
2025-09-06 21:19:53 +0100

    Neovim: Add plugins for parentheses

---
+++ nvim/.config/nvim/lua/plugins/parentheses.lua
@@ -0,0 +1,36 @@
+return {
+  "HiPhish/rainbow-delimiters.nvim",
+  {
+    "julienvincent/nvim-paredit",
+    config = function()
+      local paredit = require("nvim-paredit")
+      paredit.setup({
+        keys = {
+          ["("] = {
+            paredit.api.move_to_parent_form_start,
+            "Jump to parent form's head",
+            repeatable = false,
+            mode = { "n", "x" },
+          },
+          [")"] = {
+            paredit.api.move_to_parent_form_end,
+            "Jump to parent form's tail",
+            repeatable = false,
+            mode = { "n", "x" },
+          },
+        },
+      })
+    end
+  },
+  {
+    "m4xshen/autoclose.nvim",
+    config = function()
+      require("autoclose").setup({
+        keys = {
+          ["'"] = { disabled_filetypes = { "lisp", "rust" } },
+          ["`"] = { disabled_filetypes = { "lisp" } },
+        },
+      })
+    end
+  }
+}
