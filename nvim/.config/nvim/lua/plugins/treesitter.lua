85a53efb999ad99a6a20362ba1cbe4dce4aad555
Byron Batteson <byronbatteson@gmail.com>
2026-05-25 18:25:24 +0100

    Neovim: Upgrade dependencies to support 0.12

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -3,33 +3,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-      parser_config.p = {
-        install_info = {
-          url = "https://github.com/battesonb/tree-sitter-p",
-          branch = "main",
-          files = { "src/parser.c" },
-          queries = 'queries',
-        },
-        filetype = "p",
-      }
-
-      local configs = require("nvim-treesitter.configs")
-
-      configs.setup({
+      local config = require("nvim-treesitter.config")
+      config.setup({
         modules = {},
-        ensure_installed = {
-          "c",
-          "go",
-          "lua",
-          "markdown",
-          "markdown_inline",
-          "query",
-          "rust",
-          "typescript",
-          "vim",
-          "vimdoc",
-        },
         sync_install = false,
         auto_install = true,
         ignore_install = {},
@@ -40,6 +16,21 @@ return {
         indent = {
           enable = true,
         },
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "TSUpdate",
+        callback = function()
+          require("nvim-treesitter.parsers").p = {
+            install_info = {
+              url = "https://github.com/battesonb/tree-sitter-p",
+              branch = "main",
+              files = { "src/parser.c" },
+              queries = 'queries',
+            },
+            filetype = "p",
+          }
+        end
       })
 
       vim.opt.foldmethod = "expr"
