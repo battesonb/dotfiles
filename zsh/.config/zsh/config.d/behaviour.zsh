a027e085739ed27d8e74d6dc36d60de8c5d5cfb7
Byron Batteson <byronbatteson@gmail.com>
2026-05-27 17:52:32 +0100

    zsh: update status line
    Now includes hostname and shell level

--- zsh/.config/zsh/config.d/behaviour.zsh
+++ zsh/.config/zsh/config.d/behaviour.zsh
@@ -1,6 +1,9 @@
+# Emacs editing mode, I use tmux for vi bindings.
+bindkey -e
+
+# Emacs/Readline compatibility
 autoload -U edit-command-line
 zle -N edit-command-line
-bindkey -e
 bindkey '^xe' edit-command-line
 bindkey '^x^e' edit-command-line
 bindkey '^R' history-incremental-search-backward
--- zsh/.config/zsh/config.d/prompt.zsh
+++ zsh/.config/zsh/config.d/prompt.zsh
@@ -1,26 +1,35 @@
 # Responsible for making the prompt look pretty!
 
-function git_status() {
-    local git_branch=$(git symbolic-ref HEAD 2> /dev/null | cut -d'/' -f3)
-
-    # Prepare text
-    if [[ ! -z $git_branch ]]; then
-        local git_status=$(git status 2> /dev/null)
-        local git_root=$(basename `git rev-parse --show-toplevel 2> /dev/null` 2> /dev/null)
-        local git_color="green"
-
-        # Determine color
-        if [[ ! $git_status =~ "working tree clean" ]]; then
-            git_color="red"
-        elif [[ $git_status =~ "Your branch is ahead of" ]]; then
-            git_color="yellow"
-        fi
-
-        echo "%F{240}git[%f$git_root:%F{$git_color}$git_branch%f%F{240}]%f"
+function git_statusline() {
+    local git_branch=$(git branch --show-current 2> /dev/null)
+    if [[ -z $git_branch ]]; then
+        return
     fi
+
+    local git_status=$(git status 2> /dev/null)
+    local git_root=$(basename `git rev-parse --show-toplevel 2> /dev/null` 2> /dev/null)
+    local git_color="green"
+
+    # Determine color
+    if [[ ! $git_status =~ "working tree clean" ]]; then
+        git_color="red"
+    elif [[ $git_status =~ "Your branch is ahead of" ]]; then
+        git_color="yellow"
+    fi
+
+    echo " %F{240}git[%f$git_root:%F{$git_color}$git_branch%f%F{240}]%f"
+}
+
+function shell_level() {
+    if [[ -z $SHLVL || $SHLVL < 2 ]]; then
+        return
+    fi
+
+    echo ":$SHLVL"
 }
 
 setopt PROMPT_SUBST
-RPROMPT=''
-PROMPT='%(?.%F{green}✓.%F{red}[%?])%f %F{cyan}%~%f $(git_status)
-λ '
+RPROMPT=""
+PROMPT="%(?.%F{green}✓.%F{red}[%?])%f %F{240}[$(hostname)$(shell_level)]%f %F{cyan}%~%f$(git_statusline)
+λ "
+
