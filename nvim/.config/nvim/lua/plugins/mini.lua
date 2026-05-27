fd2678139dea84df715b9e49ed896183aa35d289
Byron Batteson <byronbatteson@gmail.com>
2026-05-27 21:59:34 +0100

    Neovim: remove mini.pairs
    The config is too much to deal with. I just want it for S-expression
    languages, but it's invasive elsewhere. I'll maybe revisit this in
    future.

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -6,40 +6,6 @@ return {
         ["mini.align"] = {},
         ["mini.icons"] = {},
         ["mini.statusline"] = {},
-        ["mini.pairs"] = {
-          config = {
-            by_filetype = {
-              rust = {
-                mappings = {
-                  ["'"] = false,
-                },
-              },
-            },
-          },
-          setup = function(config)
-            local minipairs = require("mini.pairs")
-            local base_mappings = minipairs.config.mappings
-            local by_filetype = config.by_filetype
-
-            vim.api.nvim_create_autocmd({ "BufEnter" }, {
-              desc = "mini.pairs custom config",
-              group = vim.api.nvim_create_augroup("mini-pairs-custom-config", { clear = true }),
-              callback = function()
-                local filetype_config = by_filetype[vim.bo.filetype]
-                if filetype_config then
-                  for key, value in pairs(filetype_config.mappings) do
-                    if value == false then
-                        minipairs.unmap("i", key, key .. key)
-                    else
-                      local pair_info = value or base_mappings[key]
-                      minipairs.map("i", key, pair_info)
-                    end
-                  end
-                end
-              end
-            })
-          end,
-        },
       },
     },
     config = function(_, opts)
